5ecabf6567fe34103bfff7b75eee65fee88a4a26
Dang Minh Duc <dfmcvn@icloud.com>
2024-12-26 18:07:53 +0700

    Update UserController with additional comments

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -43,6 +43,8 @@ class UserController extends Controller
 
     /**
      * Display the specified resource.
+     * 
+     * @return string JSON
      */
     public function show(Request $request)
     {
